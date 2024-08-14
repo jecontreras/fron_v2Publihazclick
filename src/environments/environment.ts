825b9f412bd4f8641f1911652a081110f760f053
jecontreras <joseeduar147@gmail.com>
2024-08-14 18:37:02 -0500

    cambiso rr

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   urlSocket: "http://localhost:3000",
-  url: "https://backpublickhazclick-8ec4bad0daad.herokuapp.com",
+  url: "http://localhost:1333",
   urlFront: "https://apilokomproaqui1-9219656b6da1.herokuapp.com",
   keyEpayco: "fd1fee1f6f008ddf010440f8c092a2cb",
   URLFILE: "https://apilokomproaqui1-9219656b6da1.herokuapp.com",
